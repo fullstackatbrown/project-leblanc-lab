b87abf7f6110b12348b51f22142ad8296dfacc66
ztameem <tameem_zaidat@brown.edu>
2024-10-06 15:41:53 -0400

    feat:added base NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import './NavBar.css';
+import React from 'react';
+
+function NavBar() {
+    return (
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <h1>MyApp</h1>
+        </div>
+        <ul className="navbar-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    );
+  }  
+
+export default NavBar;
