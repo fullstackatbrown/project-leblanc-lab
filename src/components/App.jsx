d5df85ee3e08f20ebc7fe058f1742302e5fa4014
Jimin Ryu <jimin_ryu@brown.edu>
2024-10-13 14:40:31 -0400

    astro migration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './NavBar';
+import NavBar from './NavBar.jsx';
 
 function App() {
   return (
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,13 +5,12 @@ function NavBar() {
     return (
       <nav className="navbar">
         <div className="navbar-logo">
-          <h1>MyApp</h1>
+          <h1>LeBlanc Lab</h1>
         </div>
         <ul className="navbar-links">
           <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="#about">Team</a></li>
+          <li><a href="#services">Publications</a></li>
         </ul>
       </nav>
     );
