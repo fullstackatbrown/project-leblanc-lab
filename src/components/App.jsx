d3ad92eb7df39cb8cfb4b1cb5f3f38ff99f602c3
Jimin Ryu <jimin_ryu@brown.edu>
2024-10-20 14:46:42 -0400

    separated pages in to astro files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import '../styles/App.css';
 import NavBar from './NavBar.jsx';
 
 function App() {
   return (
     <div className="App">
-      <NavBar></NavBar>
+
+      <NavBar/>
+
       <header className="App-header">
           <div className="App">
             
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import '../styles/App.css';
+import '../styles/contact.css';
 
 function Contact() {
     return(
