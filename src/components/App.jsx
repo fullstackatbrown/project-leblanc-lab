0e32a4faadf8072f5ed21491ad13e5790e3105f3
Nikolas Nemergut <165232513+NemergutN@users.noreply.github.com>
2024-10-20 20:31:57 -0400

    added main page layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,27 @@
 import '../styles/App.css';
 import NavBar from './NavBar.jsx';
 
+
 function App() {
   return (
     <div className="App">
 
       <NavBar/>
 
+      <div className='container'>
       <header className="App-header">
-          <div className="App">
+          <div className="left-section">
+            <h1>LeBlanc Lab</h1>
+            <p> Lorem Ipsum <a href="https://physics.brown.edu">Brown University </a> </p>
+            <p>lfskdlfdsf fjskfsjlfjsd j fjslkfjsdlf  jkldjflksjflsd fsjklj fjsl fsjdfkl sjf lsjflksfj dslfsj fsl</p>
+            <a href="/publications"> <button>SEE OUR PUBLICATIONS &rarr;</button></a>
             
           </div>
       </header>
+      <div className="right-section">
+        <img id="team-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Brown_Bears_Athletics_logo.svg/800px-Brown_Bears_Athletics_logo.svg.png"></img>
+      </div>
+      </div>
     </div>
   );
 }
