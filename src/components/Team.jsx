52816e2dfb589884975ac72911ca2d8fce5687af
Serena Agarwal <serenaagarwal@Serenas-MacBook-Air-3802.local>
2024-11-01 21:12:39 -0400

    formatting changes

---
+++ src/components/Team.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import './App.css';
+// import './team.css';
+
+// function Team() {
+//     return (
+//         <div>
+//             <h1 className="team-title">Team</h1>
+//             <p>This is the team page.</p>
+//         </div>
+
+//     );
+// }
+
+// export default Team;
