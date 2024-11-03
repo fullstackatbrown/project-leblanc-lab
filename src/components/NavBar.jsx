4f95d99d51894c14bb6f89eba78bfb78482dc433
Eric Cho <95654323+Eric-04@users.noreply.github.com>
2024-11-03 12:20:53 -0500

    feat: added hamburger dropdown functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,25 @@
 import '../styles/NavBar.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function NavBar({ currentPage = null }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar-empty-space"></div>
-            {/* work later to change the "active" class based on the page */}
-            <ul className="navbar-links">
+
+            <button className="hamburger-icon" onClick={toggleMenu}>
+                <div className="hamburger-line"></div>
+                <div className="hamburger-line"></div>
+                <div className="hamburger-line"></div>
+            </button>
+
+            {/* Dropdown menu */}
+            <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
                 <li><a href="/" className={currentPage === 'about' ? 'active' : ''}>about</a></li>
                 <li><a href="/team" className={currentPage === 'team' ? 'active' : ''}>team</a></li>
                 <li><a href="/publications" className={currentPage === 'publications' ? 'active' : ''}>publications</a></li>
