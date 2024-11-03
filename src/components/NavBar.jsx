9e943b6f72af50373b78022ed8b2361b8bfff5e7
Eric Cho <95654323+Eric-04@users.noreply.github.com>
2024-11-03 11:18:46 -0500

    feat: bold text for navbar on active pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,16 @@
 import '../styles/NavBar.css';
 import React from 'react';
 
-function NavBar() {
+function NavBar({ currentPage = null }) {
     return (
         <nav className="navbar">
             <div className="navbar-empty-space"></div>
             {/* work later to change the "active" class based on the page */}
             <ul className="navbar-links">
-                <li><a href="/" className="active">about</a></li>
-                <li><a href="/team">team</a></li>
-                <li><a href="/publications">publications</a></li>
-                <li><a href="/talks">talks</a></li>
+                <li><a href="/" className={currentPage === 'about' ? 'active' : ''}>about</a></li>
+                <li><a href="/team" className={currentPage === 'team' ? 'active' : ''}>team</a></li>
+                <li><a href="/publications" className={currentPage === 'publications' ? 'active' : ''}>publications</a></li>
+                <li><a href="/talks" className={currentPage === 'talks' ? 'active' : ''}>talks</a></li>
             </ul>
         </nav>
     );
