d762bd5ef209f4d104701556b1f6ec82c705016a
Jimin Ryu <jimin_ryu@brown.edu>
2024-11-10 14:50:19 -0500

    updated project structure

---
+++ src/components/Talk.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import '../styles/talks.css';
+
+function Talk() {
+    return (
+        <div>
+           
+        </div>
+
+    );
+}
+
+function Talks() {
+    return (
+        <div>
+           
+        </div>
+
+    );
+}
+
+export default Talks;
